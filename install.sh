b66a2a0c07a362f781bcfbcdd784f90b28249bab
Pierre Ayoub <pierre.ayoub@protonmail.com>
2025-02-14 11:21:39 +0100

    fix(install.sh): really use my own repo for the install script
    Before, it was only for the update mechanism

--- install.sh
+++ install.sh
@@ -12,11 +12,9 @@ make_install() {
 dir=$(mktemp -t -d git-extras-install.XXXXXXXXXX) \
     && cd "$dir" \
     && echo "Setting up 'git-extras'...." \
-    && git clone https://github.com/tj/git-extras.git &> /dev/null \
+    && git clone https://github.com/pierreay/git-extras.git &> /dev/null \
     && cd git-extras \
-    && git checkout \
-        $(git describe --tags $(git rev-list --tags --max-count=1)) \
-        &> /dev/null \
+    && git checkout dev &> /dev/null \
     && make_install \
     && rm -rf "$dir"
 
